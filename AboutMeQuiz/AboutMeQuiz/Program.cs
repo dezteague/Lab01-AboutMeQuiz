f1f8ae0f410e7d31840ec571cee106b3e8d29728
Deziree' Teague <dez.teague@gmail.com>
2019-01-08 09:55:31 -0800

    completed question 1 method using a boolean

--- AboutMeQuiz/AboutMeQuiz/Program.cs
+++ AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -6,7 +6,29 @@ namespace AboutMeQuiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Deziree Teague Quiz!");
+
+            //Question1
+            Console.WriteLine("Can you guess my nickname?");
+            string name = Console.ReadLine();
+            //convert user input into lowercase
+            string nickname = name.ToLower();
+            QuestionOne(nickname);
+        }
+
+        static bool QuestionOne(string nickname)
+        {
+            if ((nickname == "dez") || (nickname == "dezi"))
+            {
+                Console.WriteLine("Yes, you're correct!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, incorrect.  My friends and family usually call me Dez or Dezi");
+                return false;
+            }
+           
         }
     }
 }
